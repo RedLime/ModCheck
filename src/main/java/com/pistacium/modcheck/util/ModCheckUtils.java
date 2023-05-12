dc4f4f74019e6782e151e1389fe72acee712cefd
RedLime <redlimerl@gmail.com>
2023-05-13 03:52:47 +0900

    delete unused methods

--- src/main/java/com/pistacium/modcheck/util/ModCheckUtils.java
+++ src/main/java/com/pistacium/modcheck/util/ModCheckUtils.java
@@ -1,16 +1,14 @@
 package com.pistacium.modcheck.util;
 
-import com.google.gson.*;
-import com.pistacium.modcheck.ModCheck;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
-import java.util.Objects;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
 public class ModCheckUtils {
     private static final HashMap<String, String> urlReqCache = new HashMap<>();
@@ -41,39 +39,6 @@ public class ModCheckUtils {
             e.printStackTrace();
         }
         throw new IllegalAccessException("Couldn't loading url request data! check your internet status");
-    }
-
-    public static JsonObject getFabricJsonFileInJar(File jarFile) {
-        try {
-            JarFile file = new JarFile(jarFile);
-            JarEntry entry =  file.getJarEntry("fabric.mod.json");
-
-            int readBytes;
-            if (entry != null) {
-                InputStream is = file.getInputStream(entry);
-                StringBuilder jsonString = new StringBuilder();
-
-                while ((readBytes = is.read()) != -1) {
-                    jsonString.append((char) readBytes);
-                }
-
-                return JsonParser.parseString(jsonString.toString()).getAsJsonObject();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static String getAPIUrl(String url, String type) {
-        if (Objects.equals(type, "github_releases")) {
-            return "https://me.redlimerl.com/mcsr/modcheck?type=github&target=" + url.split("/")[4] + "%2F" + url.split("/")[5];
-        }
-        if (Objects.equals(type, "curseforge_files")) {
-            return "https://me.redlimerl.com/mcsr/modcheck?type=curseforge&target=" + url.split("/")[5];
-        }
-
-        return url;
     }
 
     public static Config readConfig() {
